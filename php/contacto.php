90306db32bc6f54036dc256ba0b9e8b9555eafc6
Antonio Perez <jpgtony@gmail.com>
2017-12-01 00:03:57 +0100

    doing the contact section. still in progress

---
+++ php/contacto.php
@@ -0,0 +1,45 @@
+<?php
+  $from = 'Antonio Perez <jpgtony@gmail.com>';
+  $sendTo = 'Antonio Perez <jpgtony@gmail.com>';
+  $subject = 'Contact form from your website!';
+  $fields = array('name' => 'Nombre', 'surname' => 'Apellido', 'email' => 'Email', 'phone' => 'Telefono', 'message' => 'Mensaje');
+  $okMessage = 'Contact form successfully submitted. Thank you, I will get back to you soon!';
+  $errorMessage = 'There was an error while submitting the form. Please try again later';
+
+  error_reporting(0);
+
+  try {
+
+    if(count($_POST) == 0) throw new \Exception("Form is empty");
+
+    $emailText = "You have a new message from your contact form\n=============================\n";
+
+    foreach ($_POST as $key => $value) {
+      if (isset($fields[$key])) {
+        $emailText .= '$fields[$keys]: $value\n';
+      }
+    }
+
+    $headers = array( 'Content-Type: text/plain; charset="UTF-8";',
+                      'From: ' . $from,
+                      'Reply-To: ' . $from,
+                      'Return-Path: ' . $from,
+    );
+
+    mail($sendTo, $subject, $emailText, implode('\n', $headers));
+
+    $responseArray = array('type' => 'success', 'message' => $okMessage);
+
+  } catch (Exception $e) {
+      $responseArray = array('type' => 'danger', 'message' => $errorMessage);
+  }
+
+  if(!empty($_SERVER['HTTP_X_REQUESTED-WITH']) && mb_strtolower($_SERVER['HTTP_X_REQUESTED-WITH']) == 'xmlhttprequest') {
+    $encoded = json_encode($responseArray);
+    header('Content-Type: application/json');
+    echo $encoded;
+  } else{
+    echo $responseArray['message'];
+  }
+
+?>
