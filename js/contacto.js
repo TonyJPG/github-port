aaca073df5b7dc0f217b07a830ffc2d0e5572e10
Antonio Perez <jpgtony@gmail.com>
2017-12-03 22:46:18 +0100

    Contact section is done!

--- js/contacto.js
+++ js/contacto.js
@@ -1,36 +1,23 @@
 /*version 1*/
 $(function() {
   $('#contact-form').validator();
-
   $('#contact-form').on('submit', function(e) {
     if (!e.isDefaultPrevented()) {
-      console.log("entre al if1");
-      var url = 'php/contacto.php';
-
+      var url = '../php/contacto.php';
       $.ajax({
         type: "POST",
         url: url,
         data: $(this).serialize(),
-        success: function (data)
-        {
-          console.log("entre al ajax.data");
+        success: function (data) {
           var messageAlert = 'alert-' + data.type;
           var messageText = data.message;
-          console.log(messageAlert);
-          console.log(messageText);
-
           var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
-          console.log(alertBox);
           if (messageAlert && messageText) {
-            console.log("entre al if2");
             $('#contact-form').find('.messages').html(alertBox);
             $('#contact-form')[0].reset();
-          } else {
-            console.log("NO entré al if2");
           }
         }
-      })
-      console.log("retorné falso");
+      });
       return false; //this return fix the double email and stuff
     }
   })
--- php/contacto.php
+++ php/contacto.php
@@ -1,46 +1,60 @@
-  <?php
+<?php
   /*version 1*/
-  $from = 'Antonio Perez <jpgtony@gmail.com>';
-  $sendTo = 'Antonio Perez <jpgtony@gmail.com>';
+  namespace contacto_portafolio;
+  use PHPMailer\PHPMailer\PHPMailer;
+  require 'PHPMailer-master/src/PHPMailer.php';
+  require 'PHPMailer-master/src/Exception.php';
+
+  $fromEmail = 'jpgtony@gmail.com';
+  $fromName = 'Antonio Perez';
+
+  $sendToEmail = 'jpgtony@gmail.com';
+  $sendToName = 'Antonio Perez';
+
   $subject = 'TE ESCRIBEN DESDE TU PAGINA WEB!';
   $fields = array('name' => 'Nombre', 'surname' => 'Apellido', 'email' => 'Email', 'phone' => 'Telefono', 'message' => 'Mensaje');
   $okMessage = 'Contact form successfully submitted. Thank you, I will get back to you soon!';
   $errorMessage = 'There was an error while submitting the form. Please try again later';
 
-  error_reporting(0);
+  error_reporting(0); //E_ALL & ~E_NOTICE
 
   try {
 
     if(count($_POST) == 0) throw new \Exception("Form is empty");
-
-    $emailText = "Alguien llenó el formulario de tu web!!\nPuso lo siguiente:\n=============\n\n";
+    $emailTextHtml = "<h1>Te han escrito un mensaje desde el formulario de tu web</h1><hr>";
+    $emailTextHtml .= "<table>";
 
     foreach ($_POST as $key => $value) {
       if (isset($fields[$key])) {
-        $emailText .= "$fields[$key]: $value\n";
+        $emailTextHtml .= "<tr><th>$fields[$key]</th><td>$value</td></tr>";
       }
     }
+    $emailTextHtml .= "</table><hr>";
+    $emailTextHtml .= "<p>Have a nice day,<br>Best,<br>Ondrej</p>";
 
-    $headers = array( 'Content-Type: text/plain; charset="UTF-8";',
-                      'From: ' . $from,
-                      'Reply-To: ' . $from,
-                      'Return-Path: ' . $from,
-    );
+    $mail = new PHPMailer;
 
-    mail($sendTo, $subject, $emailText, implode("\n", $headers));
+    $mail->setFrom($fromEmail, $fromName);
+    $mail->addAddress($sendToEmail,$sendToName);
 
-    $responseArray = array('type' => 'success', 'message' => $okMessage);
+    $mail->isHTML(true);
+    $mail->Subject = $subject;
+    $mail->msgHTML($emailTextHtml); // this will also create a plain-text version of the HTML email, very handy
 
-  } catch (Exception $e) {
-      $responseArray = array('type' => 'danger', 'message' => $errorMessage);
+    if (!$mail->send()) {
+      throw new \Exception('I could not send the email.' . $mail->ErrorInfo);
+    }
+
+    $responseArray = array('type' => 'info', 'message' => $okMessage);
+
+  } catch (\Exception $e) {
+      $responseArray = array('type' => 'danger', 'message' => $e->getMessage());
   }
 
-  if(!empty($_SERVER['HTTP_X_REQUESTED-WITH']) && mb_strtolower($_SERVER['HTTP_X_REQUESTED-WITH']) == 'xmlhttprequest') {
+  if(!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
     $encoded = json_encode($responseArray);
     header('Content-Type: application/json');
     echo $encoded;
-  } else{
+  } else {
     echo $responseArray['message'];
   }
-
-?>
